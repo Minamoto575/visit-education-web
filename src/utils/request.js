933d2215d74b9f008bf4905b331f2f64b8545a24
Minamoto <911190218@qq.com>
2021-11-30 20:50:06 +0800

    更新文件 request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -6,7 +6,7 @@ import { getToken } from '@/utils/auth'
 // create an axios instance
 const service = axios.create({
   // baseURL: 'http://localhost:8575',
-  baseURL: 'http://47.106.170.165:8575',
+  baseURL: 'https://47.106.170.165:8575',
   // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // timeout: 5000, // request timeout
   withCredentials: true // 允许携带cookies
